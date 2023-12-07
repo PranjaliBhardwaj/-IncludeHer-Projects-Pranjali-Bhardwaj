f8a32c4aba6ec76cd3371162409f0e5b4440de62
Pranjali Bhardwaj <146981751+PranjaliBhardwaj@users.noreply.github.com>
2023-12-07 22:38:59 +0530

    Create back.py
    Integration of Genrative AI using OpenAI.

---
+++ back.py
@@ -0,0 +1,19 @@
+import os
+import openai
+from config import key
+
+from openai import OpenAI
+
+client = OpenAI()
+openai.api_key = key
+
+response = openai.images.generate(
+    model="dall-e-3",
+    prompt="a white siamese cat",
+    size="1024x1024",
+    quality="standard",
+    n=6,
+)
+
+image_url = response.data[0].url
+print(image_url)
